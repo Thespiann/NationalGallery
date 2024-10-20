040842b2dc3b0b0c8b18f8733ebc4746cbab6b8f
Despoina Kontopoulou <despkontopoulou@gmail.com>
2024-10-20 19:30:01 +0300

    go to homepage

--- app/src/main/java/com/despkontopoulou/nationalgallery/HomepageActivity.java
+++ app/src/main/java/com/despkontopoulou/nationalgallery/HomepageActivity.java
@@ -1,6 +1,5 @@
 package com.despkontopoulou.nationalgallery;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class MainActivity extends AppCompatActivity {
+public class HomepageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_homepage);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        new Timer().schedule(new TimerTask(){
-            public void run(){
-            startActivity(new Intent(MainActivity.this, HomepageActivity.class));
-            finish();}
-        }, 5000);//wait 2s to continue to homepage
     }
 }
